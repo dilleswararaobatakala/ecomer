c1a66b4671555d2c49ccdbf58d072bb49a5dcd35
dilleswara <dilleswara.batakala@tavant.com>
2024-09-25 12:58:10 +0530

    new comads added

--- dilli.py
+++ dilli.py
@@ -1,4 +1,23 @@
 def hello():
     print("hello world i am new to github")
 
+def gitlearn():
+    print("git learnt thank for your support")
+
+
+def push_to_specfic_branch():
+    print(" to push the code from local mechine to remote that to also specific branch : git push origin dev ")
+
+def create_the_branch_in_local():
+    print(" this is command for create a branch in locally git branch -b dev")
 hello()
+
+def check_the_how_many_branches_are_avaliable():
+    print("this is the command to the how many branches are avaliable in the local : git branch ")
+
+def check_the_branches_in_the_remote_location():
+    print("this is the command to check the branches avaliable at the remote location:: git branch -r")
+
+def change_the_branch_one_branch_to_another_branch_in_locally():
+    print("this is the command to change froom one branch to another branch in local mechine : git checkout dev ") 
+change_the_branch_one_branch_to_another_branch_in_locally()
